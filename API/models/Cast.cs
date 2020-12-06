985c494006b58354613fcabbcb3ea90c3e494657
Syed Razvi <103219618@swin.edu.au>
2020-12-06 21:12:55 +1100

    instantiating

--- API/models/Cast.cs
+++ API/models/Cast.cs
@@ -6,15 +6,21 @@ namespace API.models
     public class Cast
     {
         public int CastID { get; set; }
+        public int ActorNo { get; set; }
+        public int MovieNo { get; set; }
 
         public Cast()
         {
             this.CastID = 0;
+            this.ActorNo = 0;
+            this.MovieNo = 0;
         }
 
-        public Cast(int castid)
+        public Cast(int castid, int actorno, int movieno)
         {
             this.CastID = castid;
+            this.ActorNo = actorno;
+            this.MovieNo = movieno;
         }
 
 
