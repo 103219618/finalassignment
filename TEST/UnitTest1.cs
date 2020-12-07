f5addeb537787dd29d5988fc546506114f26ca50
Syed Razvi <103219618@swin.edu.au>
2020-12-07 21:31:18 +1100

    adding sln and reference files

--- TEST/UnitTest1.cs
+++ TEST/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
+using API.models;
 using Xunit;
 
 namespace TEST
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //TEST TASK
 
+        Movie m = new Movie();
+
+        // Task a, checking output for num actors method
+        [Fact]
+        public void NumActorsTest1()
+        {
+            int MovieNo = 20;
+            Assert.Equal(1, m.NumActors(MovieNo));
+        }
+
+        [Theory]
+        [InlineData(1,21)]
+        [InlineData(1,22)]
+        [InlineData(1,23)]
+        [InlineData(1,24)]
+        public void NumActorsTest2(int expected, int movieno)
+        {
+            Assert.Equal(expected, m.NumActors(movieno));
         }
     }
 }
