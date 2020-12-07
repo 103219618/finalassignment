1f282d4f615f4a215c861103d57fb92f1e68f24b
Syed Razvi <103219618@swin.edu.au>
2020-12-07 22:08:54 +1100

    errors running test

--- TEST/UnitTest1.cs
+++ TEST/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using API.models;
 using Xunit;
+using System.Data.SqlClient;
 
 namespace TEST
 {
@@ -8,14 +9,14 @@ namespace TEST
     {
         //TEST TASK
 
-        Movie m = new Movie();
+        Movie x = new Movie();
 
         // Task a, checking output for num actors method
         [Fact]
         public void NumActorsTest1()
         {
             int movieno = 20;
-            Assert.Equal(1, m.NumActors(movieno));
+            Assert.Equal(1, x.NumActors(movieno));
         }
 
         [Theory]
@@ -25,7 +26,25 @@ namespace TEST
         [InlineData(1,24)]
         public void NumActorsTest2(int expected, int movieno)
         {
-            Assert.Equal(expected, m.NumActors(movieno));
+            Assert.Equal(expected, x.NumActors(movieno));
+        }
+
+        //Task b, checking correct output for GetAge method
+        [Fact]
+        public void GetAgeTest1()
+        {
+            int movieno = 20;
+            Assert.Equal(20, x.GetAge(movieno));
+        }
+
+        [Theory]
+        [InlineData(15,21)]
+        [InlineData(16,22)]
+        [InlineData(20,23)]
+        [InlineData(21,24)]
+        public void GetAgeTest2(int expected, int movieno)
+        {
+            Assert.Equal(expected, x.GetAge(movieno));
         }
     }
 }
